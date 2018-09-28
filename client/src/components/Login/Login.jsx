809786708003b448fb7f05282a59042a3fc5d41b
Isabelle Zhou <isabellezhou@Isabelles-MacBook-Pro.local>
2018-09-27 19:05:14 -0700

    added login page

---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component {
+  render() {
+    return (
+      <body>
+        <div className="welcome">
+          <h2>Welcome!</h2>
+          <h3>Get started here.</h3>
+          <form>
+            <input type="text" placeholder="Email" />
+            <br />
+            <input type="password" placeholder="Password" />
+          </form>
+        </div>
+      </body>
+    );
+  }
+}
+
+export default Login;
