0f6d91e0d2a05dcc61a9b823f540f856483e7cab
Kjue <kelvinjue@gmail.com>
2018-09-28 19:09:01 -0700

    added stars

--- client/src/components/Survey_components/MC/MC.jsx
+++ client/src/components/Survey_components/MC/MC.jsx
@@ -3,7 +3,7 @@ import './MC.css';
 
 function displayAnswers(answers) {
   let aList = [];
-  for (let i = 0; i < answers.length; i += 1) {
+  for (let i = 0; i < answers.length; i++) {
     aList.push(
       <label className="container">
         <input type="radio" name="mc" />
