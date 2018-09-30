369f99d043e8c1671c3ab4b2e17ca3f5ce371c5f
Carolyn Duan <carolynduan@airbears2-10-142-72-172.airbears2.1918.berkeley.edu>
2018-09-30 15:40:20 -0700

    finished cell text responses

---
+++ client/src/components/CellResponses/CellResponses.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './CellResponses.css';
+import Cell from './Cell/Cell';
+
+let array = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+
+  'Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy textLorem Ipsum is simply dummy textLorem Ipsum is simply dummy textLorem Ipsum is simply dummy text',
+  'hi',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+];
+
+// returns an array of components
+// param responses = array of text responses
+function displayResponses(responses) {
+  let i;
+  let componentArray = [];
+  let color;
+
+  for (i = 0; i < responses.length; i++) {
+    if (i % 2 === 0) color = 'grey';
+    else color = 'dark-grey';
+
+    componentArray.push(
+      <div className={color}>
+        <Cell response={responses[i]} />
+      </div>
+    );
+  }
+
+  return componentArray;
+}
+
+class CellResponses extends Component {
+  render() {
+    return (
+      <div>
+        <p className="cell-responses">
+          What was your favorite part of this unit?
+        </p>
+
+        <div className="container">{displayResponses(array)}</div>
+      </div>
+    );
+  }
+}
+
+export default CellResponses;
