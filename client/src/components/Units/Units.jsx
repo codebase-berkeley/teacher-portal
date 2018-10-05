7a12b42719cff3bb79c199e3c6c360c428f4c128
Isabelle Zhou <isabellezhou@Isabelles-MacBook-Pro.local>
2018-10-04 22:30:05 -0700

    fixed piechart and Units component

---
+++ client/src/components/Units/Units.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Unitbox from './Unitbox';
+import './Units.css';
+
+class Units extends Component {
+  render() {
+    return (
+      <div className="Page-layout">
+        <a className="Return" href="#">
+          &#8592; Return to Classes
+        </a>
+        <h2 className="Unit-header">My Units</h2>
+        <div className="Unit-titles">
+          <Unitbox unitName="House on Mango Street" />
+          <Unitbox unitName="Macbeth" />
+          <Unitbox unitName="Harry Potter and Sahana's Eyebrow" />
+          <Unitbox unitName="Harry Potter and the Gas Smell in Abhi's Apartment" />
+          <Unitbox unitName="Harry Potter and Lang's Third Nipple" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Units;
