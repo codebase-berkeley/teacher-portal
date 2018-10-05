c3be0e0f5199522acfb6daab9257b992c7872f8d
Isabelle Zhou <isabellezhou@Isabelles-MacBook-Pro.local>
2018-10-04 23:22:54 -0700

    fixing style

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -4,17 +4,20 @@ import './Login.css';
 class Login extends Component {
   render() {
     return (
-      <body>
+      <div className="login-page">
         <div className="welcome">
           <h2>Welcome!</h2>
           <h3>Get started here.</h3>
           <form>
-            <input type="text" placeholder="Email" />
-            <br />
-            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Email" className="login-box" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="login-box"
+            />
           </form>
         </div>
-      </body>
+      </div>
     );
   }
 }
