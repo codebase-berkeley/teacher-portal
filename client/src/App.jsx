9a2612a69e3191b66ee93e6f733f730f49942aa8
Bradley Chee <bradman381@gmail.com>
2018-10-07 14:11:52 -0700

    Fixed App.jsx and added react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Logout from './components/Logout/Logout';
-import Survey from './components/Survey/Survey';
+import Dashboard from './components/Dashboard/Dashboard';
 import './App.css';
 
 const answers = [
@@ -26,8 +27,12 @@ class App extends Component {
       <div className="App">
         <div className="logout-container">
           <Logout />
-          <Survey questions={answers} />
         </div>
+        <Router>
+          <Switch>
+            <Dashboard />
+          </Switch>
+        </Router>
       </div>
     );
   }
