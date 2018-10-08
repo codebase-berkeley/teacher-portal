28826e387b8a82af13cbc1bd1a779b46ce67f242
Kjue <kelvinjue@gmail.com>
2018-10-08 00:16:49 -0700

    added histogram again?

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,6 @@
 import React, { Component } from 'react';
 import Logout from './components/Logout/Logout';
-import Survey from './components/Survey/Survey';
 import './App.css';
-
-const answers = [
-  ['unit', 'Macbeth'],
-  ['section', 'Student Engagement'],
-  ['star', 'How interesting did you find this unit?'],
-  [
-    'mc',
-    'Which of the following reasons was the reason you enjoyed this unit?',
-    [
-      'Riveting textual engagement',
-      'Fascinating character dialogue',
-      'Good',
-      'Kinky'
-    ]
-  ],
-  ['frq', 'What aspects of the unit may need some work?']
-];
 
 class App extends Component {
   render() {
@@ -26,7 +8,6 @@ class App extends Component {
       <div className="App">
         <div className="logout-container">
           <Logout />
-          <Survey questions={answers} />
         </div>
       </div>
     );
--- client/src/components/Histogram/Histogram.jsx
+++ client/src/components/Histogram/Histogram.jsx
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
-import ReactChartkick, { ColumnChart } from 'react-chartkick'
-import Chart from 'chart.js'
+import ReactChartkick, { ColumnChart } from 'react-chartkick';
+import Chart from 'chart.js';
 import PropTypes from 'prop-types';
 
-ReactChartkick.addAdapter(Chart)
+ReactChartkick.addAdapter(Chart);
 
-export default class Hist extends Component {
+export default class Histogram extends Component {
   render() {
     const { info, xlabel, ylabel } = this.props;
     return (
-      <div className='histogram'>
+      <div className="histogram">
         <ColumnChart data={info} xtitle={xlabel} ytitle={ylabel} />
       </div>
     );
   }
 }
 
-Hist.propTypes = {
+Histogram.propTypes = {
   info: PropTypes.arrayOf(Array).isRequired,
   xlabel: PropTypes.string.isRequired,
   ylabel: PropTypes.string.isRequired
 };
-
