c8ba4f73363ce13f3c998bd6300587279eb224e3
Kjue <kelvinjue@gmail.com>
2018-10-11 21:51:28 -0700

    all style done

--- client/src/components/Survey/Star/Star.jsx
+++ client/src/components/Survey/Star/Star.jsx
@@ -16,65 +16,64 @@ class Star extends React.Component {
                 className="ratingControl-stars ratingControl-stars--5"
                 htmlFor="rating-5"
               >
-                            <input type="radio" id="rating-5" name="rating" value="5" />
-
-                5
+                <input type="radio" id="rating-5" name="rating" value="5" />
+                5.0
               </label>
-              <input type="radio" id="rating-45" name="rating" value="4.5" />
               <label
                 className="ratingControl-stars ratingControl-stars--45 ratingControl-stars--half"
                 htmlFor="rating-45"
               >
-                45
+                <input type="radio" id="rating-45" name="rating" value="4.5" />
+                4.5
               </label>
-              <input type="radio" id="rating-4" name="rating" value="4" />
               <label
                 className="ratingControl-stars ratingControl-stars--4"
                 htmlFor="rating-4"
               >
-                4
+                <input type="radio" id="rating-4" name="rating" value="4" />
+                4.0
               </label>
-              <input type="radio" id="rating-35" name="rating" value="3.5" />
               <label
                 className="ratingControl-stars ratingControl-stars--35 ratingControl-stars--half"
                 htmlFor="rating-35"
               >
-                35
+                <input type="radio" id="rating-35" name="rating" value="3.5" />
+                3.5
               </label>
-              <input type="radio" id="rating-3" name="rating" value="3" />
               <label
                 className="ratingControl-stars ratingControl-stars--3"
                 htmlFor="rating-3"
               >
-                3
+                <input type="radio" id="rating-3" name="rating" value="3" />
+                3.0
               </label>
-              <input type="radio" id="rating-25" name="rating" value="2.5" />
               <label
                 className="ratingControl-stars ratingControl-stars--25 ratingControl-stars--half"
                 htmlFor="rating-25"
               >
-                25
+                <input type="radio" id="rating-25" name="rating" value="2.5" />
+                2.5
               </label>
-              <input type="radio" id="rating-2" name="rating" value="2" />
               <label
                 className="ratingControl-stars ratingControl-stars--2"
                 htmlFor="rating-2"
               >
-                2
+                <input type="radio" id="rating-2" name="rating" value="2" />
+                2.0
               </label>
-              <input type="radio" id="rating-15" name="rating" value="1.5" />
               <label
                 className="ratingControl-stars ratingControl-stars--15 ratingControl-stars--half"
                 htmlFor="rating-15"
               >
-                15
+                <input type="radio" id="rating-15" name="rating" value="1.5" />
+                1.5
               </label>
-              <input type="radio" id="rating-1" name="rating" value="1" />
               <label
                 className="ratingControl-stars ratingControl-stars--1"
                 htmlFor="rating-1"
               >
-                1
+                <input type="radio" id="rating-1" name="rating" value="1" />
+                1.0
               </label>
               <input type="radio" id="rating-05" name="rating" value="0.5" />
               <label
