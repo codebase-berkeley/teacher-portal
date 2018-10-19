147e26f7fb69a3c310adb18b8ca65f6da98f1dd5
BiancaLee <biancalee27@gmail.com>
2018-10-19 00:49:57 -0700

    Finished histograms!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Logout from './components/Logout/Logout';
-import Dashboard from './components/Dashboard/Dashboard';
-import Survey from './components/Survey/Survey';
-import Units from './components/Units/Units';
+import ColumnChart from './components/Histogram/Histogram';
 import './App.css';
 
 class App extends Component {
@@ -13,13 +10,7 @@ class App extends Component {
         <div className="logout-container">
           <Logout />
         </div>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/units" component={Units} />
-            <Route exact path="/survey" component={Survey} />
-          </Switch>
-        </Router>
+        <ColumnChart />
       </div>
     );
   }
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,11 @@ router.get('/', (req, res) => {
   res.send('Welcome to the Teacher Portal API!');
 });
 
+router.get('/studentSummary', function(req, res, next) {
+  res.send([
+    { "year": 2016, "q1": 3, "q2": 4, "q3": 0, "q4": 2 }, 
+    { "year": 2017, "q1": 3, "q2": 4, "q3": 0, "q4": 2 }
+  ]);
+});
+
 module.exports = router;
