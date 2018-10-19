7e9a7175d5568d99000cb8e0625eac780491c1bf
BiancaLee <biancalee27@gmail.com>
2018-10-19 00:56:15 -0700

    Added route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Logout from './components/Logout/Logout';
-import ColumnChart from './components/Histogram/Histogram';
+import Dashboard from './components/Dashboard/Dashboard';
+import Survey from './components/Survey/Survey';
+import Units from './components/Units/Units';
+import Summary from './components/Histogram/Histogram';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +14,14 @@ class App extends Component {
         <div className="logout-container">
           <Logout />
         </div>
-        <ColumnChart />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/units" component={Units} />
+            <Route exact path="/survey" component={Survey} />
+            <Route exact path="/studentsummary" component={Summary} />
+          </Switch>
+        </Router>
       </div>
     );
   }
