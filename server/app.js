2a09b9eee181ffa6410468bdd0d909e3e7f3322a
Bradley Chee <bradman381@gmail.com>
2018-10-21 23:34:20 -0700

    Initial setup for heroku deployment

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -6,11 +7,17 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api', indexRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
+});
 
 module.exports = app;
