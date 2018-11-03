584f52f331f4e9d7a2daf43335c4964516564c1e
Kjue <kelvinjue@gmail.com>
2018-11-03 05:48:00 -0700

    functional modal on dashboard

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
 import './Modal.css';
-
 import ReactModal from 'react-modal';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
-  }
-};
+import Item from './Item';
 
 // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
 
@@ -20,13 +10,14 @@ class Modal extends Component {
   constructor() {
     super();
 
-    this.state = {
-      modalIsOpen: false
-    };
+    this.state = { modalIsOpen: false, currItem: '', items: [] };
 
     this.openModal = this.openModal.bind(this);
     this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.handleItem = this.handleItem.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.checkSubmit = this.checkSubmit.bind(this);
   }
 
   openModal() {
@@ -42,31 +33,76 @@ class Modal extends Component {
     this.setState({ modalIsOpen: false });
   }
 
+  handleItem(event) {
+    this.setState({
+      currItem: event.target.value
+    });
+  }
+
+  addItem() {
+    const { currItem, items } = this.state;
+    if (currItem !== '') {
+      this.setState({
+        currItem: ''
+      });
+      if (currItem.indexOf(',') !== 0) {
+        this.setState({
+          items: items.concat(currItem.split(','))
+        });
+      } else {
+        this.setState({
+          items: items.concat(currItem)
+        });
+      }
+    }
+  }
+
+  checkSubmit(e) {
+    if (e && e.charCode === 13) {
+      console.log(this);
+      this.addItem();
+    }
+  }
+
   render() {
-    const { modalIsOpen } = this.state;
+    const { modalIsOpen, currItem, items } = this.state;
     return (
       <div>
         <button type="button" onClick={this.openModal}>
           Open Modal
         </button>
         <ReactModal
+          className="box"
           isOpen={modalIsOpen}
           onAfterOpen={this.afterOpenModal}
           onRequestClose={this.closeModal}
-          style={customStyles}
           contentLabel="Example Modal"
+          ariaHideApp={false}
         >
-          <button type="button" onClick={this.closeModal}>
-            close
-          </button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button type="button">tab navigation</button>
-            <button type="button">stays</button>
-            <button type="button">inside</button>
-            <button type="button">the modal</button>
-          </form>
+          <h1>Add Students</h1>
+          <div className="todo-container">
+            <div className="input-container">
+              <input
+                onKeyPress={this.checkSubmit}
+                className="email-input"
+                placeholder="Add Student Email"
+                value={currItem}
+                onChange={this.handleItem}
+              />
+            </div>
+
+            <ul className="todo-item-list">
+              {items.map(item => (
+                <Item text={item} />
+              ))}
+            </ul>
+            <button type="button" onClick={this.closeModal}>
+              Back
+            </button>
+            <button type="button" onClick={this.closeModal}>
+              OK
+            </button>
+          </div>
         </ReactModal>
       </div>
     );
