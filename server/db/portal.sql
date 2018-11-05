b4ccf37a77c59eca150e294f5e686d54f4dfe14f
Richard <richardliuwa@gmail.com>
2018-11-05 15:39:49 -0800

    added content for question 4

--- server/db/portal.sql
+++ server/db/portal.sql
@@ -2,10 +2,12 @@ DROP DATABASE IF EXISTS portal;
 
 CREATE DATABASE portal;
 
-CREATE USER root WITH ENCRYPTED PASSWORD 'password';
+CREATE USER root
+WITH ENCRYPTED PASSWORD 'password';
 
 \c portal;
-CREATE TABLE users (
+CREATE TABLE users
+(
   id SERIAL PRIMARY KEY,
   email VARCHAR(254),
   first_name VARCHAR,
@@ -13,47 +15,152 @@ CREATE TABLE users (
   is_teacher BOOLEAN
 );
 
-CREATE TABLE responses (
- id SERIAL PRIMARY KEY,
- question INTEGER,
- response FLOAT,
- yr INTEGER
+CREATE TABLE responses
+(
+  id SERIAL PRIMARY KEY,
+  question INTEGER,
+  response FLOAT,
+  yr INTEGER
 );
 
 CREATE TABLE units
 (
- id SERIAL PRIMARY KEY
+  id SERIAL PRIMARY KEY
 );
 
-INSERT INTO users (email, first_name, last_name, is_teacher)
-  VALUES ('bchee@berkeley.edu', 'Bradley', 'Chee', TRUE);
+INSERT INTO users
+  (email, first_name, last_name, is_teacher)
+VALUES
+  ('bchee@berkeley.edu', 'Bradley', 'Chee', TRUE);
 
-INSERT INTO users (email, first_name, last_name, is_teacher)
-  VALUES ('parth.shah@berkeley.edu', 'Parth', 'Shah', TRUE);
+INSERT INTO users
+  (email, first_name, last_name, is_teacher)
+VALUES
+  ('parth.shah@berkeley.edu', 'Parth', 'Shah', TRUE);
 
-INSERT INTO users (email, first_name, last_name, is_teacher)
-  VALUES ('young.guo@gmail.com', 'Young', 'Guo', FALSE);
+INSERT INTO users
+  (email, first_name, last_name, is_teacher)
+VALUES
+  ('young.guo@gmail.com', 'Young', 'Guo', FALSE);
 
 -- dummy data for responses 
 
-INSERT INTO responses (question, response, yr) VALUES (1, 2, 2016);
-INSERT INTO responses (question, response, yr) VALUES (1, 4, 2016);
-INSERT INTO responses (question, response, yr) VALUES (1, 5, 2016);
-INSERT INTO responses (question, response, yr) VALUES (2, 2.5, 2016);
-INSERT INTO responses (question, response, yr) VALUES (2, 3.5, 2016);
-INSERT INTO responses (question, response, yr) VALUES (2, 4.5, 2016);
-INSERT INTO responses (question, response, yr) VALUES (3, 2.5, 2016);
-INSERT INTO responses (question, response, yr) VALUES (3, 3.5, 2016);
-INSERT INTO responses (question, response, yr) VALUES (3, 4.5, 2016);
-INSERT INTO responses (question, response, yr) VALUES (1, 4.5, 2017);
-INSERT INTO responses (question, response, yr) VALUES (1, 4.5, 2017);
-INSERT INTO responses (question, response, yr) VALUES (1, 4.5, 2017);
-INSERT INTO responses (question, response, yr) VALUES (2, 4.5, 2017);
-INSERT INTO responses (question, response, yr) VALUES (2, 4.5, 2017);
-INSERT INTO responses (question, response, yr) VALUES (2, 4.5, 2017);
-INSERT INTO responses (question, response, yr) VALUES (3, 4.5, 2017);
-INSERT INTO responses (question, response, yr) VALUES (3, 4.5, 2017);
-INSERT INTO responses (question, response, yr) VALUES (3, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (1, 2, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (1, 4, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (1, 5, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (2, 2.5, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (2, 3.5, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (2, 4.5, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (3, 2.5, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (3, 3.5, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (3, 4.5, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (4, 5.0, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (4, 2.5, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (4, 3.5, 2016);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (1, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (1, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (1, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (2, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (2, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (2, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (3, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (3, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (3, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (2, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (2, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (3, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (3, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (3, 4.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (4, 2.0, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (4, 1.5, 2017);
+INSERT INTO responses
+  (question, response, yr)
+VALUES
+  (4, 3.5, 2017);
 
 
 
