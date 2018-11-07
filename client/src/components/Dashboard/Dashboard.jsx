bb00d054c70469aa46e4da98d4492e471c62b64a
Isabelle Zhou <isabellezhou@berkeley.edu>
2018-11-07 15:48:20 -0800

    fixed the git push issue

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ClassBox from './ClassBox/ClassBox';
 import AddClassBox from './AddClassBox/AddClassBox';
-import StudentModal from '../Modal/StudentModal';
 import './Dashboard.css';
 
 const topBar = 'top-bar';
@@ -43,7 +42,6 @@ class Dashboard extends Component {
     const { classList } = this.state;
     return (
       <div className="dashboard-container">
-        <StudentModal className="add-students" />
         <p className="my-classes">My Classes</p>
         <div className="boxes-container">
           <AddClassBox />
