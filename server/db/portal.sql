a93d8fa27c8554a5507051b82d73bb336580ecd9
Kjue <kelvinjue@gmail.com>
2018-11-08 23:41:47 -0800

    refactered every table :(

--- server/db/portal.sql
+++ server/db/portal.sql
@@ -15,13 +15,6 @@ CREATE TABLE users
   is_teacher BOOLEAN
 );
 
-INSERT INTO users
-  (email, first_name, last_name, is_teacher)
-VALUES
-  ('parth.shah@berkeley.edu', 'Parth', 'Shah', TRUE),
-  ('young.guo@gmail.com', 'Young', 'Guo', FALSE
-);
-
 CREATE TABLE classes
 (
   id SERIAL PRIMARY KEY,
@@ -29,40 +22,12 @@ CREATE TABLE classes
   class_name VARCHAR
 );
 
-INSERT INTO classes
-  (teacherID, class_name)
-VALUES
-  (1, 'English 102');
-
-INSERT INTO classes
-  (teacherID, class_name)
-VALUES
-  (2, 'English 202');
-
-INSERT INTO classes
-  (teacherID, class_name)
-VALUES
-  (1, 'English 302');
-
 CREATE TABLE units
 (
   id SERIAL PRIMARY KEY,
   classid SERIAL REFERENCES classes(id),
   unit_name VARCHAR
 );
-
-INSERT INTO units
-  (classid, unit_name)
-VALUES(1, 'House on Mango Street');
-
-INSERT INTO units
-  (classid, unit_name)
-VALUES(2, 'Macbeth');
-
-INSERT INTO units
-  (classid, unit_name)
-VALUES(1, 'Jane Eyre');
-
 
 CREATE TABLE lessons
 (
@@ -72,34 +37,6 @@ CREATE TABLE lessons
   reflectionText TEXT,
   unit_id SERIAL REFERENCES units (id)
 );
-
-
-INSERT INTO lessons
-  (lesson_name, pdf, reflectionText, unit_id)
-VALUES
-  ('Volumes of Rotating Solids', 'lesson.pdf', 'blah', 1);
-
-INSERT INTO lessons
-  (lesson_name, pdf, reflectionText, unit_id)
-VALUES
-  ('Related Rates', 'lesson.pdf', 'blah', 1);
-
-INSERT INTO lessons
-  (lesson_name, pdf, reflectionText, unit_id)
-VALUES
-  ('Linear Algebra', 'lesson.pdf', 'blah', 1);
-
-INSERT INTO lessons
-  (lesson_name, pdf, reflectionText, unit_id)
-VALUES
-  ('Integrals', 'lesson.pdf', 'blah', 1);
-
-INSERT INTO lessons
-  (lesson_name, pdf, reflectionText, unit_id)
-VALUES
-  ('Differential Equations', 'lesson.pdf', 'blah', 1);
-
--- dummy data for responses 
 
 CREATE TABLE questions
 (
@@ -116,131 +53,85 @@ CREATE TABLE responses
   yr INTEGER
 );
 
-INSERT INTO questions
-  (id, text)
+-- dummy data for users
+
+INSERT INTO users
+  (email, first_name, last_name, is_teacher)
 VALUES
-  (1, 'How interesting was this unit?');
+  ('parth.shah@berkeley.edu', 'Parth', 'Shah', TRUE),
+  ('young.guo@gmail.com', 'Young', 'Guo', FALSE
+);
+
+-- dummy data for classes
+
+INSERT INTO classes
+  (teacherID, class_name)
+VALUES
+  (1, 'English 102'),
+  (2, 'English 202'),
+  (1, 'English 302');
+
+-- dummy data for units
+
+INSERT INTO units
+  (classid, unit_name)
+VALUES
+  (1, 'House on Mango Street'),
+  (2, 'Macbeth'),
+  (1, 'Jane Eyre');
+
+-- dummy data for lessons 
+
+INSERT INTO lessons
+  (lesson_name, pdf, reflectionText, unit_id)
+VALUES
+  ('Volumes of Rotating Solids', 'lesson.pdf', 'blah', 1),
+  ('Related Rates', 'lesson.pdf', 'blah', 1),
+  ('Linear Algebra', 'lesson.pdf', 'blah', 1),
+  ('Integrals', 'lesson.pdf', 'blah', 1),
+  ('Differential Equations', 'lesson.pdf', 'blah', 1);
+
+-- dummy data for questions 
 
 INSERT INTO questions
   (id, text)
 VALUES
-  (2, 'Would you recommend to a friend?');
-
-INSERT INTO questions
-  (id, text)
-VALUES
-  (3, 'Did you learn anything?');
-
-INSERT INTO questions
-  (id, text)
-VALUES
+  (1, 'How interesting was this unit?'),
+  (2, 'Would you recommend to a friend?'),
+  (3, 'Did you learn anything?'),
   (4, 'What is the purpose of life?');
 
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (1, 1, 2, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (1, 1, 4, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (1, 1, 5, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (2, 1, 2.5, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (2, 1, 3.5, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (2, 1, 4.5, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (3, 1, 2.5, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (3, 1, 3.5, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (3, 1, 4.5, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (4, 1, 5.0, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (4, 1, 2.5, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (4, 1, 3.5, 2016);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (1, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (1, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (1, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (2, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (2, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (3, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (2, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (2, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (3, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (3, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (3, 1, 4.5, 2017);
-INSERT INTO responses
-  (question, unit,response, yr)
-VALUES
-  (4, 1, 2.0, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (4, 1, 1.5, 2017);
-INSERT INTO responses
-  (question, unit, response, yr)
-VALUES
-  (4, 1, 3.5, 2017);
+-- dummy data for responses
 
+INSERT INTO responses
+  (question, unit, response, yr)
+VALUES
+  (1, 1, 2, 2016),
+  (1, 1, 4, 2016),
+  (1, 1, 5, 2016),
+  (2, 1, 2.5, 2016),
+  (2, 1, 3.5, 2016),
+  (2, 1, 4.5, 2016),
+  (3, 1, 2.5, 2016),
+  (3, 1, 3.5, 2016),
+  (3, 1, 4.5, 2016),
+  (4, 1, 5.0, 2016),
+  (4, 1, 2.5, 2016),
+  (4, 1, 3.5, 2016),
+  (1, 1, 4.5, 2017),
+  (1, 1, 4.5, 2017),
+  (1, 1, 4.5, 2017),
+  (2, 1, 4.5, 2017),
+  (2, 1, 4.5, 2017),
+  (3, 1, 4.5, 2017),
+  (2, 1, 4.5, 2017),
+  (2, 1, 4.5, 2017),
+  (3, 1, 4.5, 2017),
+  (3, 1, 4.5, 2017),
+  (3, 1, 4.5, 2017),
+  (4, 1, 2.0, 2017),
+  (4, 1, 1.5, 2017),
+  (4, 1, 3.5, 2017);
 
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO root;
