fe8f9e950f72848b8ddeb4176f319d111df5c210
Richard <richardliuwa@gmail.com>
2018-11-14 22:21:13 -0800

    survey

--- server/db/portal.sql
+++ server/db/portal.sql
@@ -108,7 +108,8 @@ VALUES
   (2, 1, 'Would you recommend to a friend?'),
   (3, 1, 'Did you learn anything?'),
   (4, 1, 'What is the purpose of life?'),
-  (5, 1, 'Why is Parth such a fucking snake?');
+  (5, 1, 'Why is Parth such a fucking snake?'),
+  (6, 1, 'lol');
 
 -- dummy data for responses
 
