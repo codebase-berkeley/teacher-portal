fef6582b3c3497545830c2052e771c877442e600
Richard <richardliuwa@gmail.com>
2018-11-14 22:11:56 -0800

    made questions dynamic!!!!!!

--- client/src/components/Histogram/Histogram.jsx
+++ client/src/components/Histogram/Histogram.jsx
@@ -25,19 +25,18 @@ export default class Histogram extends Component {
         throw new Error('Request Failed!');
       })
       .then(jsonResponse => {
-        const questions = ['q1', 'q2', 'q3', 'q4'];
-        const titles = ['Question 1', 'Question 2', 'Question 3', 'Question 4'];
+        const { num } = jsonResponse[0];
         const charts = [];
         const xlabel = 'Years';
         const ylabel = 'Average Star Rating';
-        for (let i = 0; i < questions.length; i += 1) {
+        for (let i = 0; i < num; i += 1) {
           const arr = [];
           for (let j = 0; j < jsonResponse.length; j += 1) {
-            arr.push([jsonResponse[j].year, jsonResponse[j][questions[i]]]);
+            arr.push([jsonResponse[j].year, jsonResponse[j].questions[i]]);
           }
           charts.push(
             <div>
-              <h2 className="questionTitle">{titles[i]}</h2>
+              <h2 className="questionTitle">{`Question ${i + 1}`}</h2>
               <ColumnChart data={arr} xtitle={xlabel} ytitle={ylabel} />
             </div>
           );
