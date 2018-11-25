fcdc5075be3ffc40ef509c8c775521b68a8245c7
Isabelle Zhou <isabellezhou@berkeley.edu>
2018-11-25 15:55:35 -0800

    styled the radio buttons

--- client/src/components/Survey/Star/Star.jsx
+++ client/src/components/Survey/Star/Star.jsx
@@ -10,18 +10,59 @@ class Star extends React.Component {
       <div className="star">
         {question}
 
-        <div className="container">
-          <div className="demo">
-            <input type="radio" id="rating-5" name={name} value="5" />
-            <label htmlFor="rating-5">5</label>
-            <input type="radio" id="rating-4" name={name} value="4" />
-            <label htmlFor="rating-4">4</label>
-            <input type="radio" id="rating-3" name={name} value="3" />
-            <label htmlFor="rating-3">3</label>
-            <input type="radio" id="rating-2" name={name} value="2" />
-            <label htmlFor="rating-2">2</label>
-            <input type="radio" id="rating-1" name={name} value="1" />
-            <label htmlFor="rating-1">1</label>
+        <div className="stars">
+          <div className="rating">
+            <div className="question">
+              <input
+                className="form-radio"
+                type="radio"
+                id="rating-5"
+                name={name}
+                value="5"
+              />
+              <label htmlFor="rating-5">5</label>
+            </div>
+            <div className="question">
+              <input
+                className="form-radio"
+                type="radio"
+                id="rating-4"
+                name={name}
+                value="4"
+              />
+              <label htmlFor="rating-4">4</label>
+            </div>
+            <div className="question">
+              <input
+                className="form-radio"
+                type="radio"
+                id="rating-3"
+                name={name}
+                value="3"
+              />
+              <label htmlFor="rating-3">3</label>
+            </div>
+
+            <div className="question">
+              <input
+                className="form-radio"
+                type="radio"
+                id="rating-2"
+                name={name}
+                value="2"
+              />
+              <label htmlFor="rating-2">2</label>
+            </div>
+            <div className="question">
+              <input
+                className="form-radio"
+                type="radio"
+                id="rating-1"
+                name={name}
+                value="1"
+              />
+              <label htmlFor="rating-1">1</label>
+            </div>
           </div>
         </div>
       </div>
