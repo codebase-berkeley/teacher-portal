c78b1ae6609cca1dab963892a5cd44431f361930
Isabelle Zhou <isabellezhou@berkeley.edu>
2018-11-26 23:43:08 -0800

    reformating classes for better code

--- client/src/components/Survey/Star/Star.jsx
+++ client/src/components/Survey/Star/Star.jsx
@@ -11,58 +11,66 @@ class Star extends React.Component {
         {question}
 
         <div className="stars">
-          <div className="rating">
-            <div className="star">
-              <input
-                className="form-radio"
-                type="radio"
-                id="rating-5"
-                name={name}
-                value="5"
-              />
-              <label htmlFor="rating-5">5</label>
-            </div>
-            <div className="star">
-              <input
-                className="form-radio"
-                type="radio"
-                id="rating-4"
-                name={name}
-                value="4"
-              />
-              <label htmlFor="rating-4">4</label>
-            </div>
-            <div className="star">
-              <input
-                className="form-radio"
-                type="radio"
-                id="rating-3"
-                name={name}
-                value="3"
-              />
-              <label htmlFor="rating-3">3</label>
-            </div>
+          <div className="star">
+            <input
+              className="form-radio"
+              type="radio"
+              id="rating-5"
+              name={name}
+              value="5"
+            />
+            <label className="rating" htmlFor="rating-5">
+              5
+            </label>
+          </div>
+          <div className="star">
+            <input
+              className="form-radio"
+              type="radio"
+              id="rating-4"
+              name={name}
+              value="4"
+            />
+            <label className="rating" htmlFor="rating-4">
+              4
+            </label>
+          </div>
+          <div className="star">
+            <input
+              className="form-radio"
+              type="radio"
+              id="rating-3"
+              name={name}
+              value="3"
+            />
+            <label className="rating" htmlFor="rating-3">
+              3
+            </label>
+          </div>
 
-            <div className="star">
-              <input
-                className="form-radio"
-                type="radio"
-                id="rating-2"
-                name={name}
-                value="2"
-              />
-              <label htmlFor="rating-2">2</label>
-            </div>
-            <div className="star">
-              <input
-                className="form-radio"
-                type="radio"
-                id="rating-1"
-                name={name}
-                value="1"
-              />
-              <label htmlFor="rating-1">1</label>
-            </div>
+          <div className="star">
+            <input
+              className="form-radio"
+              type="radio"
+              id="rating-2"
+              name={name}
+              value="2"
+            />
+            <label className="rating" htmlFor="rating-2">
+              2
+            </label>
+          </div>
+          <div className="star">
+            <input
+              className="form-radio"
+              type="radio"
+              id="rating-1"
+              name={name}
+              value="1"
+            />
+            <label className="rating" htmlFor="rating-1">
+              1
+            </label>
           </div>
         </div>
       </div>
