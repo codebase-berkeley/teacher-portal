da15d5260ca2a1bf2908fb296ce74df646b19756
Andrew Lieu <Andrewlieu@berkeley.edu>
2018-12-01 21:20:01 -0800

    errors fixed. tested

--- client/src/components/Units/Units.jsx
+++ client/src/components/Units/Units.jsx
@@ -19,6 +19,7 @@ class Units extends Component {
       unitList: [],
       unitModalType: true,
       unitName: '',
+      isTeacher: false,
       unitID: 0,
       questionInputs: {}
     };
@@ -47,7 +48,8 @@ class Units extends Component {
     if (units.ok) {
       const unitsJSON = await units.json();
       this.setState({
-        unitList: unitsJSON
+        unitList: unitsJSON.query,
+        isTeacher: unitsJSON.is_teacher
       });
     }
   }
@@ -81,7 +83,8 @@ class Units extends Component {
       modalIsOpen: false,
       unitModalType: true,
       questions: [],
-      questionInputs: {}
+      questionInputs: {},
+      unitName: ''
     });
   }
 
@@ -164,7 +167,7 @@ class Units extends Component {
     }));
   }
 
-  create(unitNames) {
+  create(unitNames, isTeacher) {
     this.unitBoxes = [];
     for (let i = 0; i < unitNames.length; i += 1) {
       this.unitBoxes.push(
@@ -174,6 +177,7 @@ class Units extends Component {
           id={unitNames[i].id}
           path="/lessons"
           reRender={this.componentWillMount}
+          isTeacher={isTeacher}
         />
       );
     }
@@ -191,7 +195,8 @@ class Units extends Component {
       modalIsOpen,
       unitModalType,
       questions,
-      unitName
+      unitName,
+      isTeacher
     } = this.state;
     if (unitModalType) {
       return (
@@ -216,7 +221,8 @@ class Units extends Component {
               >
                 +&nbsp;&nbsp;Add New Unit
               </button>
-              {this.create(unitList)}
+
+              {this.create(unitList, isTeacher)}
               <Modal
                 className="newUnitModal"
                 isOpen={modalIsOpen}
@@ -294,7 +300,7 @@ class Units extends Component {
           >
             +&nbsp;&nbsp;Add New Unit
           </button>
-          {this.create(unitList)}
+          {this.create(unitList, isTeacher)}
           <Modal
             className="newUnitModal Expand"
             isOpen={modalIsOpen}
